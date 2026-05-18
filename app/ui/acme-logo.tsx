d8ffd23f4aad92aa70a433fde2a4f4958330b8f6
eysafranata <eysafranata@gmail.com>
2026-05-18 17:17:36 +0700

    tiara 1

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,10 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+    <div className="flex flex-row items-center gap-3 leading-none text-white">
+      <Image src="/logo1.jpeg" alt="Logo KirimAja" width={40} height={40} className="object-contain rounded-lg" />
+      <p className="text-[28px] font-extrabold tracking-tight">KirimAja</p>
     </div>
   );
 }
